1a1dbb1ec98c530e6bb1d0fb88db74b1b01032ff
LupoArticoblu <wolf94-@libero.it>
2022-11-29 16:32:34 +0100

    tentativo di inserimento Js #1

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ let counter = 0;
 document.getElementById("js-ex").addEventListener("click", myFunction);
 
 function myFunction() {
-  
+  counter++;
+  document.getElementById("number_of_items").innerHTML = counter;
+  console.log('ciao');
 }
